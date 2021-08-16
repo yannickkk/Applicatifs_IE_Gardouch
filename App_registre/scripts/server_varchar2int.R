f44c283e0893e7199e1cf9367fa1e17267b27088
yannickkk <39738426+yannickkk@users.noreply.github.com>
2021-08-16 17:06:10 +0200

    Add files via upload

---
+++ App_registre/scripts/server_varchar2int.R
@@ -0,0 +1,21 @@
+source("C:/Users/ychaval/Documents/BD_Gardouch/Programmes/R/structure_actuelle/con_server_db_gardouch.R")
+con<-serveur_gardouch
+
+#######espece
+
+varia<-var[which(var$var_name_short =="ani_espece_id"),"var_id"]
+
+d[which(d$ii_var_id == varia),"ii_value"]<- dbGetQuery(con,paste0("SELECT obj_id FROM list.tr_object_obj WHERE obj_name = '",d[which(d$ii_var_id == varia),"ii_value"],"'"))
+
+#######parents
+
+varia<-var[which(var$var_name_short =="ani_pere"),"var_id"]
+
+d[which(d$ii_var_id == varia),"ii_value"]<- dbGetQuery(con,paste0("SELECT ani_n_inra FROM public.registre_gardouch WHERE ani_nom_registre = '",d[which(d$ii_var_id == varia),"ii_value"],"'and ani_sexe = 'M'"))
+
+varia<-var[which(var$var_name_short =="ani_mere"),"var_id"]
+
+d[which(d$ii_var_id == varia),"ii_value"]<- dbGetQuery(con,paste0("SELECT ani_n_inra FROM public.registre_gardouch WHERE ani_nom_registre = '",d[which(d$ii_var_id == varia),"ii_value"],"' and ani_sexe = 'F'"))
+
+               
+#dbDisconnect(con)
